160d105f2560d89a100488b51d915a9b9839392b
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-16 09:24:46 +0100

    [UPD] CONCATENAZIONE

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -24,7 +24,8 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+// SOLUZIONE 
+const fullName = firstName +" "+ lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
