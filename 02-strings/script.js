c5879108465f1ae8194ca136c564732c8d0da704
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-16 09:04:32 +0100

    [UPD] CREAZIONE DI TRE VALORI CON ASSEGNAZIONE

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -11,6 +11,12 @@ console.log(FullName);
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 
+// SOLUZIONE 
+const number = 10;
+let text = "libri da leggere";
+let bool = "true";
+console.log(number, text, bool);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
